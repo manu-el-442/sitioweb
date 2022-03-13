de91807aa4cb283427e5d8733937a5baaebf3100
Manuel Gallardo <85761780+manu-el-442@users.noreply.github.com>
2022-03-12 18:43:29 -0600

    Add files via upload

---
+++ administrador/index.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+if($_POST){
+  if(($_POST['usuario']=="prueba")&&($_POST['contrasenia']=="php")){
+ 
+    $_SESSION['usuario']="ok";
+    $_SESSION['nombreUsuario']="Administrador";
+    header('location:inicio.php');
+  }else{
+    $mensaje="Error: El usuario o contraseña son incorrectos";
+
+
+  }
+
+
+}
+?>
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Administrador</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+  <div class="container">
+      <div class="row">
+
+      <div class="col-md-4">
+          
+      </div>
+
+
+
+          <div class="col-md-4"><br><br><br>
+
+              <div class="card">
+                  <div class="card-header">
+                      Iniciar sesión
+                  </div>
+                  <div class="card-body">
+
+                  <?php if(isset($mensaje)) {?>
+
+                  <div class="alert alert-danger" role="alert">
+                  <?php echo $mensaje; ?>
+                  </div>
+                  <?php } ?>
+                  
+
+                    <form method="POST">
+
+
+                     <div class="form-group">
+                        <label >Usuario</label>
+                        <input type="text" class="form-control" name="usuario" id="exampleInputEmail1" placeholder="Escribe tu usuario">
+                      </div>
+
+
+                      <div class="form-group">
+                        <label >Contraseña:</label>
+                        <input type="password" class="form-control" name="contrasenia" id="exampleInputEmail1" placeholder="Escribe tu contraseña">
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary">Entrar al administrador</button>
+                        
+                      
+
+
+                    </form>
+                  
+                      
+                  </div>
+                  
+              </div>
+
+              
+          </div>
+          
+      </div>
+  </div>  
+
+
+  </body>
+</html>
