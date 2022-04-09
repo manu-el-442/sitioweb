103ac906c902f9badbe3e11b0c8857a58abf8ee1
Manuel Gallardo <85761780+manu-el-442@users.noreply.github.com>
2022-04-09 11:38:15 -0500

    Update cabecera.php

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -35,15 +35,25 @@ if(!isset($_SESSION['usuario'])){
 
   <?php $url="https://".$_SERVER['HTTP_HOST']."/" ?>
 
-      <nav class="navbar navbar-expand navbar-light bg-light">
-          <div class="nav navbar-nav">
-              <a class="nav-item nav-link active" href="#">Administrador del sitio web <span class="sr-only">(current)</span></a>
-              <a class="nav-item nav-link" href="<?php echo $url;?>/administrador/inicio.php">Inicio</a>
-              <a class="nav-item nav-link" href="<?php echo $url;?>/administrador/seccion/productos.php">Libros</a>
-              <a class="nav-item nav-link" href="<?php echo $url;?>/administrador/seccion/cerrar.php">Cerrar</a>
-              <a class="nav-item nav-link" href="<?php echo $url; ?>">Ver sitio web</a>
-          </div>
-      </nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Manuel Gallardo</a>
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Alternar navegación" _mstaria-label="420485" _mstvisible="0" _msthidden="A" _msthiddenattr="71305">
+      <span class="navbar-toggler-icon" _mstvisible="1"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="navbarColor01" style="">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $url;?>/administrador/inicio.php"><font >Administrador </font><span class="sr-only">(actual)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $url;?>/administrador/seccion/productos.php">Libros</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="<?php echo $url; ?>">Ver sitio web</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
       <div class="container">
         <br>
